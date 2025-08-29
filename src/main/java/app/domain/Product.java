3ac032b7363d390922b911e6c8d7b0bc9bccc13c
konstantin-gaubatz <konstantin.gaubatz@gmail.com>
2025-08-29 12:08:00 +0200

    domain layer implemented

---
+++ src/main/java/app/domain/Product.java
@@ -0,0 +1,37 @@
+package app.domain;
+
+import java.util.Objects;
+
+/*
+Этот класс находится на первом слое нашего проекта - домен.
+Домен описывает классы, описывающие сущности, с которыми работает приложение.
+*/
+public class Product {
+
+    private Long id;
+    private String title;
+    private double price;
+    private boolean active;
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Double.compare(price, product.price) == 0 && Objects.equals(id, product.id) && Objects.equals(title, product.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, price);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product: id - %d, title - %s, price - %.2f, active - %s",
+                id, title, price, active ? "yes" : "no");
+    }
+}
